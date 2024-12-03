678b2c38e0ca625a80f739a4df74cc85d80d81d9
deepakkandwal <kandwaldeepak72@gmail.com>
2024-12-03 15:51:33 +0530

    Add files via upload

---
+++ day7/index.js
@@ -0,0 +1,37 @@
+// console.log("----welvome in js----");
+// const title = document.getElementById("title");
+// console.log(title);
+
+// title.innerText = "hii  i'm Deepak";
+// getElementByIclassName="desc"
+// console.log("desc");
+// desc[0].innerText = "my name is Deepak"
+// const heading = document;
+// getElementByIclassName("heading");
+
+// heading[0].console
+//select
+// consth1Ele = document.getElementByclassName("title");
+
+//new content variable
+// const newContent = "-----12345-----";
+
+//assign Value to tag
+//consth1Ele.innerText = newContent;
+//         0   1  2  3,......
+//divEle = [div,div,div,div,div,...];
+
+// consth1Ele[3].innerText = "Third div Tag";
+// consthEle[1].innerText = "first div tag";
+// consthEle[2].innerText = "second div tag";
+
+//Arry
+//index : 0 1 2 3 4 5 6 7 8 9 
+const arr = [10,9,8,7,6,5,4,3,2,1];
+
+//print Specific element of an array;
+//syntax : array_name.length
+
+const firstelement = arr[0];
+
+
